11bfec304a0e90b2eff8c4f31c677da97f0284f4
Daniel Green <daniel@danielgreen.net>
2026-02-28 21:54:44 -0800

    refactor: Remove logging and unused variables from biome update patches and clean up RegionOverlayPlugin

--- src/WorldZones.Mod.RegionOverlay/Patches/PlayerUpdateBiomePatch.cs
+++ src/WorldZones.Mod.RegionOverlay/Patches/PlayerUpdateBiomePatch.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Reflection;
-using BepInEx.Logging;
 using HarmonyLib;
 
 namespace WorldZones.Mod.RegionOverlay.Patches
@@ -8,8 +7,6 @@ namespace WorldZones.Mod.RegionOverlay.Patches
     [HarmonyPatch]
     public static class PlayerUpdateBiomePatch
     {
-        internal static ManualLogSource Log { get; set; }
-
         public static event Action<float, float, long, string> BiomeUpdated;
 
         public static MethodBase TargetMethod()
@@ -29,7 +26,6 @@ namespace WorldZones.Mod.RegionOverlay.Patches
             long playerId = __instance.GetPlayerID();
             string playerName = __instance.GetPlayerName() ?? string.Empty;
 
-            Log?.LogDebug($"[PlayerUpdateBiomePatch] Postfix fired. Player pos=({position.x:F0},{position.z:F0}) id={playerId} name='{playerName}'");
             BiomeUpdated?.Invoke(position.x, position.z, playerId, playerName);
         }
     }
